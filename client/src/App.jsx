05a1ac751d45415ef43df2c802839461dd96e891
matthewyohannes <matthewyohannes77@gmail.com>
2024-02-17 15:26:56 -0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [topArtists, setTopArtists] = useState([]);
+
+
+  
+
+  useEffect(() => {
+    // Check if the user is logged in when the component mounts
+    checkLoggedIn();
+  }, []);
+
+  const checkLoggedIn = async () => {
+    try {
+      // Check if the user is logged in by making a request to Flask backend's checkLoggedIn endpoint
+      const response = await fetch('/checkLoggedIn');
+      if (response.ok) {
+        setLoggedIn(true);
+        // If user is logged in, fetch top artists
+        fetchTopArtists();
+      }
+    } catch (error) {
+      console.error('Error checking login status:', error);
+    }
+  };
+
+  const fetchTopArtists = async () => {
+    try {
+      // Fetch top artists data from Flask backend
+      const response = await fetch('/topartists');
+      if (response.ok) {
+        const data = await response.json();
+        setTopArtists(data.items);
+      }
+    } catch (error) {
+      console.error('Error fetching top artists:', error);
+    }
+  };
+  //console.log(artistsArray);
+  //console.log("hi")
+  const handleLogin = async () => {
+    // Redirect the user to the login route on your Flask backend
+    window.location.href = 'http://localhost:5000/login';
+  };
+
+  const artistInfo = topArtists.map(person =>
+    <li key={person.id}>
+      <div className="artist-card">
+        <img
+          src={person.images[2].url}
+          alt={person.name}
+          className="artist-image"
+        />
+        <div className="artist-info">
+          <p className="artist-name">{person.name}</p>
+        </div>
+      </div>
+    </li>
+  );
+
+ 
+
+
+
+
+  return (
+
+    
+      
+    <div className='artist-container'>
+    <article>
+        <h1>Welcome to Spotirank!<br/>Here are your Top Artists</h1>
+        <h5>This data is from the last 4 weeks!</h5>
+        {artistInfo}
+      </article>
+    </div>
+      
+   
+  
+  );
+};
+
+export default App;
---
+++ flask-server/server.py
@@ -0,0 +1,148 @@
+import requests
+import urllib.parse
+import base64
+import json
+
+from datetime import datetime, timedelta
+from flask import Flask, redirect, request, jsonify, session
+from flask_cors import CORS
+from credentials import MY_CLIENT_ID, MY_CLIENT_SECRET, MY_APP_SECRET_KEY
+
+
+
+app = Flask(__name__)
+CORS(app, origins='http://localhost:5173')
+app.secret_key = MY_APP_SECRET_KEY # replace w/ a secret key similar to this 7e1b8d3f-4a2c-8e5d-3bfb-0c2a5d1e4f7e
+
+
+
+
+
+CLIENT_ID = MY_CLIENT_ID # replace with CLIENTID given after creating a Spotify App on dev spotify website
+CLIENT_SECRET = MY_CLIENT_SECRET # replace with CLIENTSECRET given after creating a Spotify App on dev spotify website
+REDIRECT_URI = 'http://localhost:5000/callback' # replace with your localhost port url(if different) for your flask/python server.py
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
+
+
+@app.route('/')
+def index():
+    
+    return "Welcome to my Spotify App <a href='/login'> Login with Spotify</a>"
+
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email user-top-read' # scopes that user gives client access to
+    
+    params = { # params used to create the auth_url
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI, # used to force user to login each time (testing purposes only)
+        'show_dialog': True
+    }
+    
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url) # redirects user to this url
+
+@app.route('/callback') 
+def callback():
+    if 'error' in request.args: # checks if there is an error inside the url given after trying to authorize
+        return jsonify({"error" : request.args['error']})
+    
+    if 'code' in request.args:  # checks that code was retrieved in the url
+        req_body = { # params used to fetch access token in the POST request
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'show_dialog': True
+        }
+        
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode(),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        # makes POST request using auth code to get token
+        response = requests.post(TOKEN_URL, data=req_body, headers=headers)
+
+    
+        if response.status_code == 200:
+            token_info = response.json()
+            print("Access Token: ", token_info['access_token'])
+        else:
+            print("Error: ", response.text)
+
+
+
+        # adds token info to session storage
+        session['access_token'] = token_info['access_token'] 
+        session['refresh_token'] = token_info['refresh_token'] 
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in'] # sets time that token will expire
+
+        return redirect('http://localhost:5173')
+
+
+
+
+@app.route('/topartists')
+def get_topartists():
+    if 'access_token' not in session: # checks if access_token was successfully stored, if not then login
+        return redirect('/login')
+
+    if datetime.now().timestamp() > session['expires_at']: # checks if token has expired by using the current time
+        return redirect('/refresh-token')
+
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}" # headers for using get request to fetch access token
+    }
+    
+    response = requests.get(API_BASE_URL + 'me/top/artists?time_range=short_term&limit=10', headers=headers) # fetches playlists
+    topartists = response.json()
+    artists = topartists['items']
+
+
+    # for artist in artists: # loops through artsits inside of the `items` object array of returned data
+    #     print(artist['name'])
+    return jsonify(topartists)
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session: # checks if refresh token ever got stored, if not then login
+        return redirect('/login')
+
+    if datetime.now().timestamp() > session['expires_at']:
+
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'show_dialog': False
+        }
+    
+        response = requests.post(TOKEN_URL, data=req_body)
+        new_token_info = response.json()
+
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+        return redirect('/playlists')
+    
+@app.route('/checkLoggedIn')
+def check_logged_in():
+    if 'access_token' in session and 'expires_at' in session:
+        # Check if the access token is present and hasn't expired
+        if datetime.now().timestamp() < session['expires_at']:
+            # User is logged in
+            return jsonify({'loggedIn': True}), 200
+    # User is not logged in
+    return jsonify({'loggedIn': False}), 401
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
